bb1b06f66a86eb46d171560d32ad1d80a77ee217
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-09-16 10:07:03 -0400

    update

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import useForm from 'react-hook-form'
+import {useForm} from 'react-hook-form'
 const ContactPage = () => {
+    const {register,handleSubmit,errors} = useForm()
+    const onSubmit = (data) => {
+  console.log(data)
+    }
   return (
-    <div>
-      contact-page
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="email" name="email" ref={register}/>
+        <input type='password' placeholder="Password" name="password" ref={register}/>
+        <input type='submit'/>
+     
+    </form>
   );
 }
 
-export default ContactPage;
+export default ContactPage ;
