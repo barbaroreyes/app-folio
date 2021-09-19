584a5d2d97a844faee76d4e33cbe6153e30334d6
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-09-18 21:30:33 -0400

    added-link-live

--- src/Compo/Project.js
+++ src/Compo/Project.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 const Project = () => {
   return (
     <div>
       Project
+    <h1>name</h1>
+    <Link to=''>GitHub</Link>
+    <Link to=''>App</Link>
     </div>
   );
 }
