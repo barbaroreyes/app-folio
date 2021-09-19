1f0526d71976f8f0cd176f6e224ae3d75da568c2
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-09-18 21:32:44 -0400

    update-lists

--- src/Pages/Portafolio.js
+++ src/Pages/Portafolio.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Project from '../Compo/Project'
 const Portafolio = () => {
   return (
     <div className="portafolio">
       <h2>Projects</h2>
-      
+       <Project/>
     </div>
   )
 }
